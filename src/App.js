d177efc4c43a4854dba7ad440dd5604de9801263
SrinivasaKamathR <kamathsri1997@gmail.com>
2023-01-07 22:32:02 +0530

    adding buttons functionality to show and hide the cart items

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Cart from "./components/Cart/Cart";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCarthandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHanlder = () => {
+    setCartIsShown(false);
+  };
   return (
     <React.Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHanlder} />}
+      <Header onShowCart={showCarthandler} />
       <main>
         <Meals />
       </main>
